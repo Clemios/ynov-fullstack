d532563d49cf20df4d18c829f5388cc20fb1c4e4
Clemios <clem_ios@hotmail.com>
2025-06-18 14:55:20 +0200

    core: ajout des BDD fix needed

--- user-service/src/routes/serveRoutes.ts
+++ user-service/src/routes/serveRoutes.ts
@@ -7,13 +7,21 @@ const generateRoutes = (app: Application) => {
         const { url, reqType, callback } = route;
         if (reqType === 'GET') {
             // Handle GET request
-            app.get(url, callback);
+            // app.post(url, callback);            
+            app.get(url, (req: Request, res: Response, next: NextFunction) => {
+                console.log(`GET request received at ${url}`)});
         } else if (reqType === 'POST') {
-            app.post(url, callback);            
+            // app.post(url, callback);
+            app.post(url, (req: Request, res: Response, next: NextFunction) => {
+                console.log(`POST request received at ${url}`)});            
         } else if (reqType === 'PUT') {
-            app.put(url, callback);                  
+            // app.put(url, callback); 
+            app.put(url, (req: Request, res: Response, next: NextFunction) => {
+                console.log(`PUT request received at ${url}`)});                 
         } else if (reqType === 'DELETE') {
-            app.delete(url, callback);         
+            // app.delete(url, callback); 
+            app.delete(url, (req: Request, res: Response, next: NextFunction) => {
+                console.log(`PUT request received at ${url}`)});         
         } else {
             console.log(`No specific request type defined for ${url}`);
         }
