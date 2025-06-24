8a543e5fa54705ccc0d10a6877f7bc332c9682bc
Clemios <clem_ios@hotmail.com>
2025-06-24 20:35:48 +0200

    fix: clean code

--- user-service/src/services/auth.ts
+++ user-service/src/services/auth.ts
@@ -12,19 +12,15 @@ interface User extends NewUser {
   id: string;
 }
 
-// Use in-memory store for now
-const users: User[] = [];
-
 export const signup = async ({ name, email, password }: Omit<User, 'id'>) => {
   const existingUser = await userService.getUserByEmail(email);
-  // const existing = users.find((u) => u.email === email);
+
   if (existingUser) return null;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
   const newUser: NewUser = {
     name,
     email,
-    password: hashedPassword
+    password
   };
   console.log('New user payload:', newUser);
 
@@ -41,14 +37,11 @@ export const login = async (email: string, password: string) => {
   const user = await userService.getUserByEmail(email);
   console.log('User pass:', password);
   console.log('User found:', user);
-  if (!user) return null; // pas d'unser avec cet email
+  if (!user) return null; // pas d'user avec cet email
 
   const valid = await bcrypt.compare(password, user.password);
-  console.log('Password sent:', password);
-  console.log('hash in db: $2b$10$DvOwE9oIUp8GU7ZDsCwxKOpgkFvg.ZYUOOYuVri0swOno2J2Tr.6i');
-  // $2y$10$UjUuqOg0p0oN51pt9PatGO24obHK9INPcRt19O9PXdF14XmtFWoIu
   if (!valid) return null; // mot de passe incorrect
-  console.log('Password valid:', valid);
+
   // creation JWT
   const token = signToken({ id: user.id, email });
 
